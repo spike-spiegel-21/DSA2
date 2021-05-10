082643618827c622744017154f4c35880c8564d8
Mayank Solanki <bizmiku@gmail.com>
2021-05-10 13:21:57 +0530

    Dijkstra added

---
+++ Graphs/Dijkstra's Algorithm.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+int findMinVertex(int* distance, bool* visited, int n){
+    int minVertex=-1;
+    for(int i=0; i<n; i++){
+        if(!visited[i] && (minVertex==-1 || distance[i]<distance[minVertex])){
+            minVertex=i;
+        }    
+    }
+    return minVertex;
+}
+
+void dijkstra(int** edges, int n, bool* visited){
+    int* distance = new int[n];
+    for(int i=0; i<n; i++){
+        distance[i]=INT32_MAX;
+    }
+    distance[0]=0;
+
+    for(int i=0; i<n-1; i++){
+        int minVertex = findMinVertex(distance, visited, n);
+        visited[minVertex]=1;
+        for(int j=0; j<n; j++){
+            if(edges[minVertex][j]>0){
+                distance[j]=min(distance[j], distance[minVertex]+edges[minVertex][j]);
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<i<<" "<<distance[i]<<endl;
+    }
+
+}
+int main() {
+    int n; 
+    int e;
+    cin>>n>>e; 
+
+    int** edges = new int*[n];
+
+    for(int i=0; i<n; i++){
+        edges[i] = new int[n];
+        for(int j=0; j<n; j++){
+            edges[i][j]=0;
+        }
+    }
+
+    for(int i=0; i<e; i++){
+        int f,s,w;
+        cin>>f>>s>>w;
+        edges[f][s]=w;
+        edges[s][f]=w;
+    }
+
+    int sv=0;
+    bool* visited = new bool[n];
+    for(int i=0; i<n; i++){
+        visited[i]=0;
+    }
+
+    dijkstra(edges, n, visited);
+
+    
+}
+
