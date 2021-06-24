6ebcaf9274fb66d6f4e4f28159d962cfa6a29e4e
Mayank Solanki <bizmiku@gmail.com>
2021-06-24 22:27:00 +0530

    sudo commit

--- Graphs/Dijkstra's Algorithm.cpp
+++ Graphs/Dijkstra's Algorithm.cpp
@@ -1,67 +1,77 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int findMinVertex(int* distance, bool* visited, int n){
-    int minVertex=-1;
-    for(int i=0; i<n; i++){
-        if(!visited[i] && (minVertex==-1 || distance[i]<distance[minVertex])){
-            minVertex=i;
-        }    
+int findMinVertex(int *distance, bool *visited, int n)
+{
+    int minVertex = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i] && (minVertex == -1 || distance[i] < distance[minVertex]))
+        {
+            minVertex = i;
+        }
     }
     return minVertex;
 }
 
-void dijkstra(int** edges, int n, bool* visited){
-    int* distance = new int[n];
-    for(int i=0; i<n; i++){
-        distance[i]=INT32_MAX;
+void dijkstra(int **edges, int n, bool *visited)
+{
+    int *distance = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        distance[i] = INT32_MAX;
     }
-    distance[0]=0;
+    distance[0] = 0;
 
-    for(int i=0; i<n-1; i++){
+    for (int i = 0; i < n - 1; i++)
+    {
         int minVertex = findMinVertex(distance, visited, n);
-        visited[minVertex]=1;
-        for(int j=0; j<n; j++){
-            if(edges[minVertex][j]>0){
-                distance[j]=min(distance[j], distance[minVertex]+edges[minVertex][j]);
+        visited[minVertex] = 1;
+        for (int j = 0; j < n; j++)
+        {
+            if (edges[minVertex][j] > 0)
+            {
+                distance[j] = min(distance[j], distance[minVertex] + edges[minVertex][j]);
             }
         }
     }
 
-    for(int i=0; i<n; i++){
-        cout<<i<<" "<<distance[i]<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " " << distance[i] << endl;
     }
-
 }
-int main() {
-    int n; 
+int main()
+{
+    int n;
     int e;
-    cin>>n>>e; 
+    cin >> n >> e;
 
-    int** edges = new int*[n];
+    int **edges = new int *[n];
 
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         edges[i] = new int[n];
-        for(int j=0; j<n; j++){
-            edges[i][j]=0;
+        for (int j = 0; j < n; j++)
+        {
+            edges[i][j] = 0;
         }
     }
 
-    for(int i=0; i<e; i++){
-        int f,s,w;
-        cin>>f>>s>>w;
-        edges[f][s]=w;
-        edges[s][f]=w;
+    for (int i = 0; i < e; i++)
+    {
+        int f, s, w;
+        cin >> f >> s >> w;
+        edges[f][s] = w;
+        edges[s][f] = w;
     }
 
-    int sv=0;
-    bool* visited = new bool[n];
-    for(int i=0; i<n; i++){
-        visited[i]=0;
+    int sv = 0;
+    bool *visited = new bool[n];
+    for (int i = 0; i < n; i++)
+    {
+        visited[i] = 0;
     }
 
     dijkstra(edges, n, visited);
-
-    
 }
-
